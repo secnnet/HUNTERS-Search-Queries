928c6c30cc3e0f96fba27cdce06f0c5755ea9372
Bi <17622687+secnnet@users.noreply.github.com>
2023-04-15 19:20:21 +0100

    Add files via upload

--- OTHERS.sql
+++ OTHERS.sql
@@ -7,4 +7,22 @@
 ---Alter Time Zone
 ALTER SESSION SET TIMEZONE = 'UTC';
 ---------------------------------------------------------------------------------------------------------
+--The given code is an SQL query written in the AQL (Analytics Query Language) syntax, which is used to query data stored in databases, specifically for analytics purposes.
 
+--The query is selecting data from a specific table "ZSCALER_ZIA" in the database "ICON_EU_DB" and performing three calculations.
+
+--The first calculation uses the MAX function to find the maximum value of the "EVENT_TIME" column and renames it as "max_event_time".
+
+--The second calculation uses the CURRENT_TIMESTAMP function to get the current time and renames it as "current_time".
+
+--The third calculation uses the TIMESTAMPDIFF function to find the difference between the maximum value of "EVENT_TIME" and the current time, in minutes, and renames it as "lag_in_minutes".
+
+--This query can be used to track the lag time between the latest event time in the "ZSCALER_ZIA" table and the current time.
+
+--Source Lag Time
+SELECT 
+    MAX(EVENT_TIME) AS max_event_time, -- select the maximum value of the EVENT_TIME column and rename it as max_event_time
+    CURRENT_TIMESTAMP AS current_time, -- get the current timestamp and rename it as current_time
+    TIMESTAMPDIFF(MINUTES, MAX(EVENT_TIME), CURRENT_TIMESTAMP) AS lag_in_minutes -- calculate the difference between the maximum EVENT_TIME and current timestamp, in minutes, and rename it as lag_in_minutes
+FROM 
+    "ICON_EU_DB"."RAW"."ZSCALER_ZIA"; -- specify the database, schema, and table to query data from, in this case "ZSCALER_ZIA"
